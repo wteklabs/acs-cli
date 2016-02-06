94dfdc4a07ac1aaeaa5dbf5857edbb97a0bbd395
Ross Gardler <ross@gardler.org>
2016-02-06 00:29:01 -0800

    Fix method call to get agent DNS

--- acs/test/MesosTest.py
+++ acs/test/MesosTest.py
@@ -27,16 +27,19 @@ class MesosTest:
         self.log.debug(self.acs.marathonCommand('groups', 'POST', data))
         self.log.debug("End")
 
-        self.log.debug("Check the application is running")
+        url = "http://" + self.acs.getAgentsFQDN()
+        self.log.debug("Check the application is running and accessible at " + url)
         for i in range (0,10):
             self.log.debug("Attempt to access service " + str(i) + " of 10")
             try:
-                r = requests.get("http://" + self.acs.getAgentsFQDN(config))
+                r = requests.get(url)
                 if r.status_code == 200:
                     self.log.debug("Got a 200 response from the application")
                     break
             except:
-                self.log.debug("Attempt failed, sleeping for 5 seconds")
+                e = sys.exc_info()[0]
+                self.log.debug("Attempt failed: " + str(e))
+                self.log.debug("Sleeping for 5 seconds")
                 time.sleep (5)
 
         if i >= 9: 
@@ -44,7 +47,7 @@ class MesosTest:
         self.log.info("End")
 
         self.log.info("Remove the app")
-        self.acs.marathonCommand('groups/azure', 'DELETE')
+        self.acs.marathonCommand('groups/azure?force=true', 'DELETE')
         self.log.info("End")
 
 

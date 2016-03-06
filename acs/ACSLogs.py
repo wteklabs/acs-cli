7cf38237ff8cdef2553af16ffa7ecf5672a615a7
Ross Gardler <ross@gardler.org>
2016-03-06 01:54:58 -0800

    timestamp the log output

--- acs/ACSLogs.py
+++ acs/ACSLogs.py
@@ -11,21 +11,21 @@ class ACSLog:
             # create console handler and set level to info
             handler = logging.StreamHandler()
             handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter("%(levelname)s - %(message)s")
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
             
             # create error file handler and set level to error
             handler = logging.FileHandler(os.path.join(output_dir, "error.log"),"w", encoding=None, delay="true")
             handler.setLevel(logging.ERROR)
-            formatter = logging.Formatter("%(levelname)s - %(message)s")
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
             
             # create debug file handler and set level to debug
             handler = logging.FileHandler(os.path.join(output_dir, "all.log"),"w")
             handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter("%(levelname)s - %(message)s")
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
 

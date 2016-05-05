77803d62bb4d04610c91090149b98b4afac42a0b
Ross Gardler <ross@gardler.org>
2016-05-05 08:01:58 -0700

    refactor to remove some code duplication

--- acs/AgentPool.py
+++ acs/AgentPool.py
@@ -17,24 +17,37 @@ class AgentPool:
         defaults = {"orchestratorType": "Mesos"}
         self.config = config
 
-    def getNICs(self):
-        """
-        Get a list of NICs attached to an agent pool
-        """
+    def getAccessToken(self):
         tenant_id = self.config.get('Subscription', 'tenant_id')
         app_id = self.config.get('Subscription', 'app_id')
         app_secret = self.config.get('Subscription', 'app_secret')
+        return azurerm.get_access_token(tenant_id, app_id, app_secret)
+
+    def getPools(self):
+        """
+        Get a list of all the agent pools in this resource group
+        """
         subscription_id = self.config.get('Subscription', 'subscription_id')
         rg_name = self.config.get('Group', 'name')
-        access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+        access_token = self.getAccessToken()
 
         # TODO: This assumes only a single VMSS in the resource group, this will not always be the
         # case and will never be the case if when there are multiple Agent Pools
         vmss_list = azurerm.list_vm_scale_sets(access_token, subscription_id, rg_name)['value']
         self.log.debug("List of VMSS: " + json.dumps(vmss_list, indent=True))
+        return vmss_list
+
+    def getNICs(self):
+        """
+        Get a list of NICs attached to an agent pool
+        """
+        vmss_list = self.getPools()
         vmss_name = vmss_list[0]['name']
         self.log.debug("Looking up VMs in VMSS called " + vmss_name + " (if this is wrong maybe it is because AgentPool.py currently only supports a single VMSS)")
 
+        subscription_id = self.config.get('Subscription', 'subscription_id')
+        rg_name = self.config.get('Group', 'name')
+        access_token = self.getAccessToken()
         nics = azurerm.get_vmss_nics(access_token, subscription_id, rg_name, vmss_name)
         self.log.debug("List of VMSS NICs: " + json.dumps(nics, indent=True))
         return nics["value"]
@@ -43,21 +56,14 @@ class AgentPool:
         """
         Get a list of all agents in the Pool.
         """
-        tenant_id = self.config.get('Subscription', 'tenant_id')
-        app_id = self.config.get('Subscription', 'app_id')
-        app_secret = self.config.get('Subscription', 'app_secret')
+        vmss_list = self.getPools()
+        vmss_name = vmss_list[0]['name']
+        self.log.debug("Looking up VMs in VMSS called " + vmss_name + " (if this is wrong maybe it is because AgentPool.py currently only supports a single VMSS)")
+
         subscription_id = self.config.get('Subscription', 'subscription_id')
-        rgname = self.config.get('Group', 'name')
-        access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
-
-        # TODO: This assumes only a single VMSS in the resource group, this will not always be the
-        # case and will never be the case if when there are multiple Agent Pools
-        vmsslist = azurerm.list_vm_scale_sets(access_token, subscription_id, rgname)['value']
-        self.log.debug("List of VMSS: " + json.dumps(vmsslist, indent=True))
-        vmssname = vmsslist[0]['name']
-        self.log.debug("Looking up VMs in VMSS called " + vmssname + " (if this is wrong maybe it is because AgentPool.py currently only supports a single VMSS)")
-
-        vms = azurerm.list_vmss_vms(access_token, subscription_id, rgname, vmssname)
+        rg_name = self.config.get('Group', 'name')
+        access_token = self.getAccessToken()
+        vms = azurerm.list_vmss_vms(access_token, subscription_id, rgname, vmss_name)
         return vms['value']
 
     def scale(self, capacity):

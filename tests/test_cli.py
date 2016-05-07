70575807aa5075b60d59b2a28915820ffc6b74d5
Ross Gardler <ross@gardler.org>
2016-05-06 22:21:42 -0700

    move to pytest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,18 @@
 """tests for the main acs CLI module"""
 
-from subprocess import check_output
-from unittest import TestCase
-
 from acs import __version__ as VERSION
 
-class TestHelp(TestCase):
+from subprocess import check_output
+import pytest
+
+class TestCLI():
     def test_returns_usage_information(self):
         output = check_output(['acs', '-h'])
-        self.assertTrue('Usage:' in output.decode("utf-8"))
+        assert 'Usage:' in output.decode("utf-8")
 
         output = check_output(['acs', '--help'])
-        self.assertTrue('Usage:' in output.decode("utf-8"))
+        assert 'Usage:' in output.decode("utf-8")
 
-class TestVersion(TestCase):
     def test_returns_version_informatio(self):
         output = check_output(['acs', '--version'])
-        self.assertEqual(output.strip().decode("utf-8"), VERSION)
+        assert output.strip().decode("utf-8") == VERSION

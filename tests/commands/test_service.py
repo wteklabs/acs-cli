074cffe97e897333f5b577167a2c15a817163c07
Ross Gardler <ross@gardler.org>
2016-05-06 17:59:23 -0700

    move to pytest

--- tests/commands/test_service.py
+++ tests/commands/test_service.py
@@ -1,19 +1,24 @@
 """Tests for `acs service` command."""
 
 from subprocess import check_output
-from unittest import TestCase
+from acs import commands
+from acs.commands.base import Config
 
-class TestService(TestCase):
+import pytest
+
+class TestService():
 
 # Thia test is very slow, so we are not running it right now
 #    def test_create_exits_succesfully(self):
 #        output = self.execute(['acs', 'service', 'create'])
 #        pass
 
-    def test_show(self):
-        output = self._execute(['acs', 'service', 'show'])
-        self.assertTrue("azure.com" in output)
+    def test_show(self, service):
+        result = service.show()
+        assert "rgacstestdcos" in result
+        assert "azure.com" in result
 
-    def _execute(self, command):
-        result = check_output(command)
-        return result.decode("utf-8")
+    @pytest.fixture
+    def service(self):
+        config = Config("tests/test_dcos_cluster.ini")
+        return commands.Service(config, None)

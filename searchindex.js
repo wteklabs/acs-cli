6bd22588a3b75b59a8c29041fbd5cf8b5ed40e30
Ross Gardler <ross@gardler.org>
2016-05-10 22:12:30 -0700

    Generated gh-pages for commit 15663ff Author: Ross Gardler <ross@gardler.org>
        update for latest GIT syntax

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Azure Container Service Tools Documentation&#8217;s documentation!"],titleterms:{azur:0,contain:0,document:0,indic:0,servic:0,tabl:0,tool:0,welcom:0}})

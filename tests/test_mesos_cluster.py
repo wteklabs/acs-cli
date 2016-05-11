20a504c738dbeb3847067a292ed8072f32e3e024
Ross Gardler <ross@gardler.org>
2016-05-10 21:27:25 -0700

    test a deployment

--- tests/test_mesos_cluster.py
+++ tests/test_mesos_cluster.py
@@ -1,4 +1,4 @@
-"""Tests for `acs afs` subcommand."""
+"""Tests for `service` subcommand."""
 
 import pytest
 
@@ -10,5 +10,30 @@ class TestAfs():
     )
 
     def test_deployment(self, service):
+        response = service.marathonCommand('apps')
+        assert '{"apps":[]}' in response
+
+        with open ('marathon-app.json', "r") as marathonfile:
+            data=marathonfile.read().replace('\n', '').replace("\"", "\\\"")
+        response = service.marathonCommand('groups', 'POST', data)
+
+        url = "http://" + service.getAgentsFQDN()
+
+        for i in range (0,10):
+            self.log.debug("Attempt to access service " + str(i) + " of 10")
+            try:
+                r = requests.get(url)
+                if r.status_code == 200:
+                    self.log.debug("Got a 200 response from the application")
+                    break
+            except:
+                e = sys.exc_info()[0]
+                self.log.debug("Attempt failed: " + str(e))
+                self.log.debug("Sleeping for 5 seconds")
+                time.sleep (5)
+
+        assert i >= 9
+
+        service.marathonCommand('groups/azure?force=true', 'DELETE')
         response = service.marathonCommand('apps')
         assert '{"apps":[]}' in response

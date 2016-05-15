deab9f4747dc40a41505348d7eb3a83b8d97f316
Ross Gardler <ross@gardler.org>
2016-05-15 02:32:37 -0700

    test the number of agents and nics against the number in the config file not a hard coded number

--- tests/test_agentPool.py
+++ tests/test_agentPool.py
@@ -8,12 +8,12 @@ class TestAgentPool():
         num_pools = len(pools)
         assert num_pools == 2
 
-    def test_getAgents(self, agentPool):
+    def test_getAgents(self, agentPool, config):
         agents = agentPool.getAgents();
         num_agents = len(agents)
-        assert num_agents == 5
+        assert num_agents == config.get("ACS", "agentCount")
 
-    def test_getNICs(self, agentPool):
+    def test_getNICs(self, agentPool, config):
         nics = agentPool.getNICs();
         num_nics = len(nics)
-        assert num_nics == 5
+        assert num_nics == config.get("ACS", "agentCount")

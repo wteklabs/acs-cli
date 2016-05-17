f766ca73b15b93790b46c0801ddefe7eb6277257
Ross Gardler <ross@gardler.org>
2016-05-16 17:20:07 -0700

    add some documentation and helper scripts for working with Docker

---
+++ scripts/run-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RUNNING=$(docker inspect -f {{.State.Running}} acs 2> /dev/null) 
+
+if [ "$?" -ne "0" ]
+then
+  docker run -it -v $(pwd):/src -u `id -u $USER` --name acs rgardler/acs  
+  exit 0
+fi
+
+if [ "$RUNNING" -eq "true" ]
+then
+  echo "Connecting to running 'acs' container"
+  docker exec -it acs bash
+else
+  echo "Re-starting and connecting to an 'acs' container"
+  docker start acs
+  docker exec -it acs bash
+fi
+
+

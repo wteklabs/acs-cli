16e87e9a495a639bae178ac5af9c3f2313fb67ee
Ross Gardler <ross@gardler.org>
2016-05-17 21:44:35 -0700

    test service creation

--- tests/commands/test_service.py
+++ tests/commands/test_service.py
@@ -3,14 +3,39 @@
 from acs.AgentPool import AgentPool
 
 import pytest
+import time
 
 class TestService():
+
+    max_deploy_time = 25 # minutes
 
     slow = pytest.mark.skipif(
         not pytest.config.getoption("--runslow"),
         reason="need --runslow option to run"
     )
 
+    @slow
+    def test_create(self, service):
+        if service.exists():
+            service.log.debug("The test ACS cluster already exists, deleting")
+            service.delete(True)
+        exists = service.exists()
+        assert not exists
+
+        starttime = time.time()
+        service.create()
+        assert service.exists()
+        endtime = time.time()
+        service.log.info("Total deployment time: " + str((endtime - starttime)/60) + " minutes")
+
+        dns_up = False
+        start_time = time.time()
+        duration = 0
+        while not dns_up and duration < (2 * 60):
+            dns_up = service.exists()
+            duration- time.time() - start_time
+        assert dns_up, "DNS for the masters did not seem to come up"
+            
     def test_exists(self, service):
         exists = service.exists()
         assert exists
@@ -20,6 +45,7 @@ class TestService():
         assert "rgacstestdcos" in result
         assert "azure.com" in result
 
+    @slow
     def test_scale(self, service):
         initial_agents = service.config.getint('ACS', 'agentCount')
         service.args = {'--agents': initial_agents + 1}

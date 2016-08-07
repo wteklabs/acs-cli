c68e17ee3dbbb6c5a700b6d3c8ab9bbc741af67a
Ross Gardler <ross@gardler.org>
2016-08-07 01:06:47 +0000

    Add command to remove an application. Refactor the parsing of the config file

--- acs/commands/app.py
+++ acs/commands/app.py
@@ -7,6 +7,7 @@ Usage:
 
 Commands:
   deploy                deploy an application described by the appropriate app configuration
+  remove                remove an application described by the appropriate app configuration from the cluster
 
 Options:
   --app-config=<file>   the application configuration (compose file for Docker Swarm, Marhon JSON for DC/OS).
@@ -20,6 +21,7 @@ Help:
 
 from docopt import docopt
 from inspect import getmembers, ismethod
+import json
 from json import dumps
 import os
 import subprocess as sub 
@@ -27,11 +29,12 @@ import subprocess as sub
 from .base import Base
 
 class App(Base):
+  app_config_dir = "~/.acs/app/"
 
   def run(self):
     args = docopt(__doc__, argv=self.options)
-    # print("Command args")
-    # print(args)
+    # self.log.debug("App options:" + str(self.options))
+    # self.log.debug("App args:" + str(args))
     self.args = args
 
     command = self.args["<command>"]
@@ -51,17 +54,19 @@ class App(Base):
   def help(self):
     print(__doc__)
 
-  def deploy(self):
-    app_config_dir = "~/.acs/app/"
-
-    config_path = self.args["--app-config"]
+  def parseAppConfig(self, config_path):
+    """
+    Parse a use provided application configuration, replacing any
+    tokens that appear within it as appropriate. The parsed file
+    will be stored in `~/.acs/app/config/`.
+    """
     if not config_path:
       self.log.error("--app-config not supplied, unable to deploy application")
-      return "Must provide an application config file as '--app-config'"
-    
+      raise IOError("Must provide an application config file as '--app-config'")
+
     self.log.debug("Deploying application described by " + config_path)
     config_filename = os.path.expanduser(config_path)
-    perm_filename = os.path.expanduser(app_config_dir + config_filename)
+    perm_filename = os.path.expanduser(self.app_config_dir + config_filename)
     os.makedirs(os.path.dirname(perm_filename), exist_ok=True)
 
     tmpl = open(config_filename)
@@ -73,11 +78,42 @@ class App(Base):
     tmpl.close()
     output.close()
 
+    return perm_filename
+    
+  def deploy(self):
+    config_path = self.args["--app-config"]
+    try:
+      perm_filename = self.parseAppConfig(config_path)
+    except IOError as e:
+      self.log.error(str(e))
+      return "Unable to deploy application. There is a problem with the app configuration file. See the log for full details."
+
     p = sub.Popen(['dcos', 'marathon', 'app', "add", perm_filename],stdout=sub.PIPE,stderr=sub.PIPE)
     output, errors = p.communicate()
     if errors:
-      self.log.error("Error deploying applicatoin:\n" + errors.decode("utf-8"))
+      self.log.error("Error deploying application:\n" + errors.decode("utf-8"))
       return "Unable to deploy application, see log for full details."
     self.log.debug("Application deployed. Configuration stored in " + perm_filename)
 
     return "Application deployed"
+
+  def remove(self):
+    config_path = self.args["--app-config"]
+    try:
+      perm_filename = self.parseAppConfig(config_path)
+      with open(perm_filename) as app_config: 
+        app = json.load(app_config)
+        app_id = app["id"]
+    except IOError as e:
+      self.log.error(e)
+      return "Unable to remove application because: " + str(e) + ". See the log for full details."
+
+    self.log.debug("Removing app with the ID " + app_id)
+    p = sub.Popen(['dcos', 'marathon', 'app', "remove", app_id],stdout=sub.PIPE,stderr=sub.PIPE)
+    output, errors = p.communicate()
+    if errors:
+      self.log.error("Error removing application:\n" + errors.decode("utf-8"))
+      return "Unable to remove application, see log for full details."
+    self.log.debug("Application removed. Configuration stored in " + perm_filename)
+
+    return "Application removed."

d40f1bbd360c69a663c5765f02ed2160f409ef2d
Ross Gardler <ross@gardler.org>
2016-08-08 06:20:51 +0000

    add a slow lbweb test that creates the cluster and add a test for an active cluster in the faster test, issue #47

--- tests/commands/test_demo.py
+++ tests/commands/test_demo.py
@@ -8,7 +8,14 @@ class TestDemo():
         reason="need --runslow option to run"
     )
 
-    def test_lbweb(self, demo):
+    @slow
+    def test_lbweb_slow(self, demo, service):
+        """Tests the creation of a cluster and the lbweb demo. This version of the test will delete any pre-existing service.
+        """
+        if service.exists():
+            service.log.debug("The test ACS cluster already exists, deleting")
+            service.delete(True)
+
         demo.args = {'<command>': 'lbweb',
                      "--remove": False}
         try:
@@ -17,6 +24,26 @@ class TestDemo():
         except RuntimeWarning as e:
             demo.log.warning("The application was already installed so the test was not as thorough as it could have been")
 
+        
+        # remove the appliction
+        demo.args["--remove"] = True
+        result = demo.lbweb()
+        assert("Application removed" in result)
+
+        
+    def test_lbweb(self, demo, service):
+        """Tests the creation of the lbweb demo. This version of the test will fail if the test cluster dows not already exist.
+
+        """
+        assert(service.exists())
+
+        demo.args = {'<command>': 'lbweb',
+                     "--remove": False}
+        try:
+            result = demo.lbweb()
+            assert("Application deployed" in result)
+        except RuntimeWarning as e:
+            demo.log.warning("The application was already installed so the test was not as thorough as it could have been")
         
         # remove the appliction
         demo.args["--remove"] = True

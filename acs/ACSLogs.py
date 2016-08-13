04ce35342b14df3ef8d158dade5434e206ca9cd0
Ross Gardler <ross@gardler.org>
2016-08-13 19:41:41 +0000

    add line numbers to log output

--- acs/ACSLogs.py
+++ acs/ACSLogs.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 import os
 
@@ -9,26 +10,29 @@ class ACSLog:
 
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        
+
         if (not self.logger.handlers):
             # create console handler and set level to info
             handler = logging.StreamHandler()
             handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s : %(lineno)d in %(filename)s : %(message)s')
+            #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
             
             # create error file handler and set level to error
             handler = logging.FileHandler(os.path.join(output_dir, "error.log"),"w", encoding=None, delay="true")
             handler.setLevel(logging.ERROR)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s : %(lineno)d in %(filename)s : %(message)s')
+            #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
             
             # create debug file handler and set level to debug
             handler = logging.FileHandler(os.path.join(output_dir, "all.log"),"w")
             handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s : %(lineno)d in %(filename)s : %(message)s')
+            #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
 
